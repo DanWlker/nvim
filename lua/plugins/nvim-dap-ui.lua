cc1cd73cb27c37af1ff48ae764a0b04376b6a38b
DanWlker <danielhee2@gmail.com>
2025-10-14 23:41:48 +0800

    progress

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -1,6 +1,40 @@
 return {
   'rcarriga/nvim-dap-ui',
   lazy = true,
-  -- NOTE: This is commented on purpose
-  -- opts = {},
+  keys = {
+    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+    {
+      '<F7>',
+      function() require('dapui').toggle() end,
+      desc = 'Debug: See last session result.',
+    },
+  },
+  config = function()
+    local dapui = require('dapui')
+    local dap = require('dap')
+
+    dapui.setup({
+      -- Set icons to characters that are more likely to work in every terminal.
+      --    Feel free to remove or use ones that you like more! :)
+      --    Don't feel like these are good choices.
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+      },
+    })
+
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+  end,
 }
