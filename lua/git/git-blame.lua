ce3b4cfd30032236b2de076183585162e3b9452e
DanWlker <danielhee2@gmail.com>
2025-10-31 23:56:41 +0800

    add git-blame

---
+++ lua/git/git-blame.lua
@@ -0,0 +1,36 @@
+vim.g.gitblame_enabled = 0
+-- vim.g.gitblame_display_virtual_text = 0
+-- vim.g.gitblame_schedule_event = 'CursorHold'
+-- vim.g.gitblame_clear_event = 'CursorHoldI'
+
+return {
+  'f-person/git-blame.nvim',
+  keys = {
+    {
+      '<leader>gcs',
+      '<cmd>GitBlameCopySHA<cr>',
+      desc = 'Git Copy SHA',
+    },
+    {
+      '<leader>gcf',
+      '<cmd>GitBlameCopyFileURL<cr>',
+      desc = 'Git Copy File URL',
+    },
+    {
+      '<leader>gcc',
+      '<cmd>GitBlameCopyCommitURL<cr>',
+      desc = 'Git Copy Commit URL',
+    },
+
+    {
+      '<leader>gof',
+      '<cmd>GitBlameOpenFileURL<cr>',
+      desc = 'Git Open File URL',
+    },
+    {
+      '<leader>goc',
+      '<cmd>GitBlameOpenCommitURL<cr>',
+      desc = 'Git Open Commit URL',
+    },
+  },
+}
