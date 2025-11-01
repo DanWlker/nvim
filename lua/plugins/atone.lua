b872cd7198f7b11f28b928b543fd5709e6ec7195
DanWlker <danielhee2@gmail.com>
2025-11-01 23:49:13 +0800

    switch to atone

---
+++ lua/plugins/atone.lua
@@ -0,0 +1,12 @@
+return {
+  'XXiaoA/atone.nvim',
+  cmd = 'Atone',
+  opts = {}, -- your configuration here
+  keys = {
+    {
+      '<leader>tu',
+      '<cmd>Atone toggle<cr>',
+      desc = 'Toggle undo tree',
+    },
+  },
+}
