f7e2806b1e2f609151d1a7a7bbcba3e1dd89b48b
DanWlker <danielhee2@gmail.com>
2025-11-30 13:52:04 +0800

    fix incorrect variable, add dependency link

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -3,11 +3,13 @@ return {
   version = 'v2.*',
   lazy = true,
   build = 'make install_jsregexp',
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+  },
   config = function()
     local luasnip = require('luasnip')
-    local luasnip_loaders_vscode = require('luasnip.loaders.from_lua')
 
-    luasnip_loaders_vscode.lazy_load()
+    require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.filetype_extend('javascriptreact', { 'html' })
     luasnip.filetype_extend('typescriptreact', { 'html' })
     luasnip.filetype_extend('svelte', { 'html' })
@@ -16,6 +18,6 @@ return {
     luasnip.filetype_extend('javascript', { 'javascriptreact' })
     luasnip.filetype_extend('typescript', { 'typescriptreact' })
     luasnip.filetype_extend('dart', { 'flutter' })
-    luasnip_loaders_vscode.load({ paths = { './snippets' } })
+    require('luasnip.loaders.from_lua').load({ paths = { './snippets' } })
   end,
 }
