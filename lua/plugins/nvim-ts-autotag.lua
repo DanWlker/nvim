33f7a24897914ab07e4201e8b51d2dae9a39ab5a
DanWlker <danielhee2@gmail.com>
2025-12-29 00:02:07 +0800

    add ts autotag and remove autopairs

---
+++ lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,6 @@
+return {
+  'windwp/nvim-ts-autotag',
+  -- https://github.com/windwp/nvim-ts-autotag?tab=readme-ov-file#a-note-on-lazy-loading
+  event = { 'BufReadPre', 'BufNewFile' },
+  opts = {},
+}
