12a8161079eedce9436c1d237663c6da4e2ad83d
DanWlker <danielhee2@gmail.com>
2026-01-15 20:51:56 +0800

    update snippets

--- snippets/go.lua
+++ snippets/go.lua
@@ -2,8 +2,7 @@ return {
   s({ trig = 'iferr', name = 'If err != nil snippet' }, {
     t('if '),
     i(1, 'err'),
-    t({ ' != nil {', '\t' }),
-    t('return '),
+    t({ ' != nil {', '\treturn ' }),
     i(2, 'err'),
     t({ '\t', '}' }),
   }),
--- snippets/lua.lua
+++ snippets/lua.lua
@@ -4,4 +4,11 @@ return {
     i(1, 'message'),
     t('))'),
   }),
+  s({ trig = 'con', name = 'Config key for lazy nvim' }, {
+    t({ 'config = function()', '\trequire("' }),
+    i(1, ''),
+    t('").setup'),
+    i(2, '()'),
+    t({ '', 'end' }),
+  }),
 }
