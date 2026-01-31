6d89055391eefb5bb4d66700b7b1ca80571ceb03
DanWlker <danielhee2@gmail.com>
2026-02-01 01:34:33 +0800

    progress

--- lua/lang/csvview-nvim.lua
+++ lua/lang/csvview-nvim.lua
@@ -9,10 +9,10 @@ return {
       -- Use <Tab> and <S-Tab> to move horizontally between fields.
       -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
       -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
-      jump_next_field_end = { '<Tab>', mode = { 'n', 'v' } },
-      jump_prev_field_end = { '<S-Tab>', mode = { 'n', 'v' } },
-      jump_next_row = { '<Enter>', mode = { 'n', 'v' } },
-      jump_prev_row = { '<S-Enter>', mode = { 'n', 'v' } },
+      jump_next_field_end = { '<Tab>', mode = { 'n', 'x' } },
+      jump_prev_field_end = { '<S-Tab>', mode = { 'n', 'x' } },
+      jump_next_row = { '<Enter>', mode = { 'n', 'x' } },
+      jump_prev_row = { '<S-Enter>', mode = { 'n', 'x' } },
     },
   },
   ft = {
